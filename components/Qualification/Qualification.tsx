eb2f0ec87175d382f34d75b7cc486a72eb30ee5a
felix fedronic <fedronicfelix@gmail.com>
2023-04-13 02:15:52 +0200

    qualification

---
+++ components/Qualification/Qualification.tsx
@@ -0,0 +1,271 @@
+import React from "react";
+
+function Qualification() {
+  return (
+    <div className="container-fluid py-5" id="qualification">
+      <div className="container">
+        <div className="position-relative d-flex align-items-center justify-content-center">
+          <h1
+            className="display-1 text-uppercase text-white"
+            style={{ WebkitTextStroke: "1px #dee2e6" }}
+          >
+            Qualification
+          </h1>
+          <h1 className="position-absolute text-uppercase text-primary">
+            Formation & Experience
+          </h1>
+        </div>
+        <div className="row align-items-start">
+          <div className="col-lg-6">
+            <h3 className="mb-4">Ma Formation</h3>
+            <div className="border-left border-primary pt-2 pl-4 ml-2">
+              <div className="position-relative mb-4">
+                <i
+                  className="far fa-dot-circle text-primary position-absolute"
+                  style={{ top: "2px", left: "-32px" }}
+                ></i>
+                <h5 className="font-weight-bold mb-1">
+                  Titre RNCP bac+2 Développeur Web
+                </h5>
+                <p className="mb-2">
+                  <strong>OpenClassrooms</strong> |{" "}
+                  <small>Février 2021 - Février 2022</small>
+                </p>
+                <p>
+                  Formation effectuée à distance hors temps de travail. Le
+                  rythme a été intense mais tellement formateur. Merci OC !
+                </p>
+                <a
+                  href="/documents/rncp.pdf"
+                  target="_blank"
+                  className="btn btn-outline-primary"
+                >
+                  Voir le Certificat
+                </a>
+              </div>
+            </div>
+            <div className="border-left border-primary pt-2 pl-4 ml-2">
+              <div className="position-relative mb-4">
+                <i
+                  className="far fa-dot-circle text-primary position-absolute"
+                  style={{ top: "2px", left: "-32px" }}
+                ></i>
+                <h5 className="font-weight-bold mb-1">
+                  Créer votre site avec WordPress
+                </h5>
+                <p className="mb-2">
+                  <strong>Udemy</strong> |{" "}
+                  <small>Avril 2022 - Avril 2022</small>
+                </p>
+                <p>9h de formation en e-learning.</p>
+                <a
+                  href="https://www.udemy.com/certificate/UC-58b13f6c-5410-4582-a082-951cc1964b63/"
+                  target="_blank"
+                  className="btn btn-outline-primary"
+                >
+                  Voir le Certificat
+                </a>
+              </div>
+            </div>
+            <div className="border-left border-primary pt-2 pl-4 ml-2">
+              <div className="position-relative mb-4">
+                <i
+                  className="far fa-dot-circle text-primary position-absolute"
+                  style={{ top: "2px", left: "-32px" }}
+                ></i>
+                <h5 className="font-weight-bold mb-1">Next.js & React.js</h5>
+                <p className="mb-2">
+                  <strong>Udemy</strong> | <small>Mai 2022 - Juin 2022</small>
+                </p>
+                <p>25h de formation en e-learning.</p>
+                <a
+                  href="https://www.udemy.com/certificate/UC-27f1d15b-e0f5-4d0c-82aa-85006ee831e9/"
+                  target="_blank"
+                  className="btn btn-outline-primary"
+                >
+                  Voir le Certificat
+                </a>
+              </div>
+            </div>
+            <div className="border-left border-primary pt-2 pl-4 ml-2">
+              <div className="position-relative mb-4">
+                <i
+                  className="far fa-dot-circle text-primary position-absolute"
+                  style={{ top: "2px", left: "-32px" }}
+                ></i>
+                <h5 className="font-weight-bold mb-1">
+                  Docker & Kubernetes 2022 Edition
+                </h5>
+                <p className="mb-2">
+                  <strong>Udemy</strong> |{" "}
+                  <small>Juin 2022 - Juillet 2022</small>
+                </p>
+                <p>23.5h de formation en e-learning.</p>
+                <a
+                  href="https://www.udemy.com/certificate/UC-2ee7f30d-0b13-4c45-93ec-ad782a787f92/"
+                  target="_blank"
+                  className="btn btn-outline-primary"
+                >
+                  Voir le Certificat
+                </a>
+              </div>
+            </div>
+            <div className="border-left border-primary pt-2 pl-4 ml-2">
+              <div className="position-relative mb-4">
+                <i
+                  className="far fa-dot-circle text-primary position-absolute"
+                  style={{ top: "2px", left: "-32px" }}
+                ></i>
+                <h5 className="font-weight-bold mb-1">
+                  Understanding TypeScript{" "}
+                </h5>
+                <p className="mb-2">
+                  <strong>Udemy</strong> | <small>Aout 2022 - Aout 2022</small>
+                </p>
+                <p>15h de formation en e-learning.</p>
+                <a
+                  href="https://www.udemy.com/certificate/UC-5e418c56-eea2-4dc4-9e21-4e3c750f894a/"
+                  target="_blank"
+                  className="btn btn-outline-primary"
+                >
+                  Voir le Certificat
+                </a>
+              </div>
+            </div>
+            <div className="border-left border-primary pt-2 pl-4 ml-2">
+              <div className="position-relative mb-4">
+                <i
+                  className="far fa-dot-circle text-primary position-absolute"
+                  style={{ top: "2px", left: "-32px" }}
+                ></i>
+                <h5 className="font-weight-bold mb-1">Angular 2022 Edition</h5>
+                <p className="mb-2">
+                  <strong>Udemy</strong> |{" "}
+                  <small>Septembre 2022 - Octobre 2022</small>
+                </p>
+                <p>34.5h de formation en e-learning.</p>
+                <a
+                  href="https://www.udemy.com/certificate/UC-59b383f2-f2c1-45c0-acfd-04413710eb13/"
+                  target="_blank"
+                  className="btn btn-outline-primary"
+                >
+                  Voir le Certificat
+                </a>
+              </div>
+            </div>
+            <div className="border-left border-primary pt-2 pl-4 ml-2">
+              <div className="position-relative mb-4">
+                <i
+                  className="far fa-dot-circle text-primary position-absolute"
+                  style={{ top: "2px", left: "-32px" }}
+                ></i>
+                <h5 className="font-weight-bold mb-1">PHP CMS Project</h5>
+                <p className="mb-2">
+                  <strong>Udemy</strong> |{" "}
+                  <small>Décembre 2022 - Janvier 2023</small>
+                </p>
+                <p>37h de formation en e-learning.</p>
+                <a
+                  href="https://www.udemy.com/certificate/UC-e1ee4d1a-51e8-43f5-8463-9442bda6cbf2/"
+                  target="_blank"
+                  className="btn btn-outline-primary"
+                >
+                  Voir le Certificat
+                </a>
+              </div>
+            </div>
+            <div className="border-left border-primary pt-2 pl-4 ml-2">
+              <div className="position-relative mb-4">
+                <i
+                  className="far fa-dot-circle text-primary position-absolute"
+                  style={{ top: "2px", left: "-32px" }}
+                ></i>
+                <h5 className="font-weight-bold mb-1">
+                  Symfony Web Development
+                </h5>
+                <p className="mb-2">
+                  <strong>Udemy</strong> |{" "}
+                  <small>Février 2023 - Avril 2023</small>
+                </p>
+                <p>24.5h de formation en e-learning.</p>
+                <a
+                  href="https://www.udemy.com/certificate/UC-f2161ffb-81a3-4a4a-9131-064dc882bb1b/"
+                  target="_blank"
+                  className="btn btn-outline-primary"
+                >
+                  Voir le Certificat
+                </a>
+              </div>
+            </div>
+            <div className="border-left border-primary pt-2 pl-4 ml-2">
+              <div className="position-relative mb-4">
+                <i
+                  className="far fa-dot-circle text-primary position-absolute"
+                  style={{ top: "2px", left: "-32px" }}
+                ></i>
+                <h5 className="font-weight-bold mb-1">
+                  {" "}
+                  Licence (bac+3) Science de la Vie et la Santé
+                </h5>
+                <p className="mb-2">
+                  <strong>Université Paul Sabatier</strong>
+                </p>
+                <p>
+                  Mention Biologie Cellulaire et Physiologie - Université Paul
+                  Sabatier
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-6">
+            <h3 className="mb-4">Mon Experience</h3>
+            <div className="border-left border-primary pt-2 pl-4 ml-2">
+              <div className="position-relative mb-4">
+                <i
+                  className="far fa-dot-circle text-primary position-absolute"
+                  style={{ top: "2px", left: "-32px" }}
+                ></i>
+                <h5 className="font-weight-bold mb-1">
+                  Facteur Qualité - missions de management
+                </h5>
+                <p className="mb-2">
+                  <strong>Centre Courrier de l'Union</strong> | <small>Depuis 2007</small>
+                </p>
+                <ul>
+                  <li className="mb-1">
+                    Assurer la mise à jour du Système d'Information :
+                    maintenance évolutive de 10000 points de distribution,
+                  </li>
+                  <li className="mb-1">
+                    Contribuer à la satisfaction clients : analyse des incidents
+                    et corrections, veille au respect des standards des
+                    prestations de services,
+                  </li>
+                  <li className="mb-1">
+                    Participer à la gestion et au suivi de l'activité de
+                    production (22 agents sur site).
+                  </li>
+                </ul>
+              </div>
+              <div className="position-relative mb-4">
+                <i
+                  className="far fa-dot-circle text-primary position-absolute"
+                  style={{ top: "2px", left: "-32px" }}
+                ></i>
+                <h5 className="font-weight-bold mb-1">
+                  Assistant ingénieur de recherche
+                </h5>
+                <p className="mb-2">
+                  <strong>CNRS, Microbiologie et Génétique</strong> | <small>Mars 2008 - Juin 2008 </small>
+                </p>
+               
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Qualification;
